0ee686fe817bffd0f3269b812e0757b2b6e64416
harry <yuanhua_zhong@icloud.com>
2017-06-13 15:26:08 +0800

    调整项目结构。

---
+++ Image/Class/Other/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Image
+//
+//  Created by harry on 2017/6/13.
+//  Copyright © 2017年 harry. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
